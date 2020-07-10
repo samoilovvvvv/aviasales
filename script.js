1cd58328e690fd402e439063669431d53dbd7ddb
samoilovvvvv <44649842+samoilovvvvv@users.noreply.github.com>
2020-07-10 18:57:08 +0300

    added dropdown cities

--- script.js
+++ script.js
@@ -7,6 +7,52 @@ const formSearch = document.querySelector('.form_search'),
 
 const city = ['Москва', 'Киев', 'Минск', 'Афины', 'Волгоград', 'Санкт-Петербург', 'Красноярск',
 'Краснодар', 'Варшава', 'Рига', 'Бруклин', 'Дрезден', 'Одесса', 'Берлин', 'Барселона', 'Прага', 'Вена', 'Рим',
-'Гамбург', 'Геленджик', 'Дубай', 'Екатеринбург', 'Казань', 'Кёльн', 'Киев', 'Мадрид', 'Милан', 'Керч', 'Кёльн',
+'Гамбург', 'Геленджик', 'Дубай', 'Екатеринбург', 'Казань', 'Кёльн', 'Мадрид', 'Милан', 'Керч', 'Кёльн',
 'Омск', 'Осло', 'Витебск', 'Брест', 'Сочи', 'София', 'Тбилиси', 'Харьков', 'Цюрих', 'Челябинск', 'Чебоксары', 
 'Шанхай', 'Штутгарт', 'Юрмала', 'Якутск', 'Ялта', 'Ярославль'];
+
+function showCity(input, list) {
+    removeCity(document.querySelectorAll('.dropdown__city'));
+
+    if(input.value !== ''){
+
+    const filterCity = city.filter(item => {
+        const fixItem = item.toLowerCase();
+        return fixItem.includes(input.value.toLowerCase());
+    });
+
+    filterCity.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('dropdown__city');
+        li.textContent = item;
+        list.append(li);
+    });
+    }
+}
+
+function removeCity(cities) {
+    cities.forEach(item => item.remove());
+}
+
+function changeCity(event, input) {
+    const target = event.target;
+    input.value = target.textContent;
+    input.focus();
+    removeCity(document.querySelectorAll('.dropdown__city'));
+}
+
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+inputCitiesTo.addEventListener('input', () => {
+    showCity(inputCitiesTo, dropdownCitiesTo);
+});
+
+dropdownCitiesFrom.addEventListener('click', event => {
+    changeCity(event, inputCitiesFrom);
+});
+
+dropdownCitiesTo.addEventListener('click', event => {
+    changeCity(event, inputCitiesTo);
+});
